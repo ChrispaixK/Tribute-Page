bd84a91f756051296a79e4ebea343ce1f90e0c06
Chrispaix Kaze <kchrispaixs@gmail.com>
2023-10-02 11:49:53 +0200

    add User

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,16 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def index
     @articles = Article.all
   end
   def show
-    @inkuru = Article.find(params[:id])
   end
   def new
     @article = Article.new
   end
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_param)
     @article.save
     if @article.save
       flash[:notice] = "Article was created successfully"
@@ -20,11 +21,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+
+    if @article.update(article_param)
       flash[:notice] = "Article was updated successfully."
       redirect_to @article
     else
@@ -33,8 +33,16 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+  def article_param
+    params.require(:article).permit(:title, :description)
   end
 end
